a2958154ceabf0576cf54bf4196c245fcfbaf943
yoshiya12x <hikowaiide@gmail.com>
2019-03-30 11:49:27 +0900

    arrange overall

--- src/base/yoppie.go
+++ src/base/yoppie.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("はろーよっぴー")
+const (
+	ENDPOINT = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+	API_KEY  = "hoge"
+)
+
+func main() {
+	fmt.Println(ENDPOINT)
+	flag.Parse()
+	searchWord := flag.Arg(0)
+	execApi(searchWord)
+	fmt.Println(searchWord)
+}
+
+func execApi(searchWord string) {
+	// Create new http request
+	req, err := http.NewRequest("GET", ENDPOINT, nil)
+	errorHandling(err)
+
+	// Add get parameters
+	params :=req.URL.Query()
+}
+
+func errorHandling(err error) {
+	if err != nil{
+		panic(err)
+	}
 }
